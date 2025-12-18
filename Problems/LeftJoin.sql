87fac2ceaf8aa0744741b7b65cbdf10319071f04
CIPL1759 <CIPL1759@CIPL-ATC00591>
2025-12-18 20:00:21 +0530

    Add project files.

---
+++ Problems/LeftJoin.sql
@@ -0,0 +1,28 @@
+﻿
+/* Left Join */ /* */
+
+select p.name,a.state from persons as p
+left join address as a on p.id=a.personId;
+
+/* CORRELATED SUBQUERY */
+
+select p.name,(select state from address where personId = p.id) as statee
+from persons as p
+
+/* Using case and exist */
+
+select p.id, p.name,
+case when exists(select 1 from address where personId =p.id)
+ then (select state from address where personId=p.id)
+ else NULL
+ end as state
+from persons as p
+
+/*outer apply */
+SELECT p.name, s.state
+FROM persons AS p
+OUTER APPLY (
+    SELECT state 
+    FROM address 
+    WHERE personId = p.id
+) s;
---
+++ SQLQuery#.sql
@@ -0,0 +1,30 @@
+﻿select * from tbl_emp_details
+create procedure sp_details
+@param int as begin
+select * from tbl_emp_details where EmpId =@param end
+
+alter Procedure sp_PrintevenNo
+@input int
+as 
+begin
+ Declare @StartNo int
+ set @StartNo =1
+
+ while(@StartNo < @input)
+ begin
+   if(@StartNo % 2 = 0)
+    begin
+	  Print @StartNo
+	    
+	End
+	Set @StartNo = @StartNo + 1
+   END
+   Print 'Finished Printing even number till' + RTRIM(@input)
+
+END
+
+Declare @input int
+set @input = 20
+execute sp_PrintevenNo @input
+Print 'Done'
+exec sp_details 121
